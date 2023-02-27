f1d5dd5800b18f603681b54ffd09d328f11448f5
VickyMM03 <104647845+VickyMM03@users.noreply.github.com>
2023-02-27 10:45:13 +0100

    Add files via upload

---
+++ MyLittlePlanet-main/src/simulator/model/MovingTowardsFixedPoint.java
@@ -0,0 +1,31 @@
+package simulator.model;
+
+import java.util.List;
+import simulator.model.Body;
+import simulator.misc.Vector2D;
+
+public class MovingTowardsFixedPoint implements ForceLaws{
+
+	Vector2D c; //punto fijo
+	double g; //aceleracion
+	
+	public MovingTowardsFixedPoint (Vector2D c, double g){
+		if (c == null || g < 0) {
+			throw new IllegalArgumentException();
+		}
+		else {
+			this.c = c;
+			this.g = g;
+		}
+	}
+
+	@Override
+	public void apply(List<Body> bs) {
+		// TODO Auto-generated method stub
+		Vector2D d;
+		for(Body b: bs) {
+			d = c.minus(b.getPosition());
+			b.f = d.scale(b.getMass()*g);
+		}
+	}
+}
