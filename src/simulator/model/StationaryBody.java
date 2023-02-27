99982dd9cc25f44d55c4a9e29343feea023689ed
fernandoclz <114144548+fernandoclz@users.noreply.github.com>
2023-02-27 09:08:19 +0100

    Update StationaryBody.java
    No le pasamos velocidad

--- src/simulator/model/StationaryBody.java
+++ src/simulator/model/StationaryBody.java
@@ -4,7 +4,7 @@ import simulator.misc.Vector2D;
 
 public class StationaryBody extends Body{
 
-	public StationaryBody(String id, String gid, Vector2D v, Vector2D p, double m) throws IllegalArgumentException {
+	public StationaryBody(String id, String gid, Vector2D p, double m) throws IllegalArgumentException {
 		super(id, gid, new Vector2D(), p, m);
 		// TODO Auto-generated constructor stub
 	}
