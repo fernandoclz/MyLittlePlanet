f705e0732d78d193ecba9643b9d44066541a52b5
fernandoclz <114144548+fernandoclz@users.noreply.github.com>
2023-02-28 19:56:30 +0100

    Add files via upload

--- src/simulator/factories/NoForceBuilder.java
+++ src/simulator/factories/NoForceBuilder.java
@@ -3,22 +3,22 @@ package simulator.factories;
 import org.json.JSONObject;
 
 import simulator.model.ForceLaws;
+import simulator.model.NoForce;
 
 public class NoForceBuilder extends Builder<ForceLaws>{
 
 	
 	public NoForceBuilder(){
-		super(null, null);
+		super("nf", "NoTieneFuerza");
 	}
 	public NoForceBuilder(String typeTag, String desc) {
 		super(typeTag, desc);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	protected ForceLaws createInstance(JSONObject data) {
 		// TODO Auto-generated method stub
-		return null;
+		return new NoForce();
 	}
 
 }
