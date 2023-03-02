ce79679736f5d5ba15bbf90d0f17bc4d220dfdf6
fernandoclz <114144548+fernandoclz@users.noreply.github.com>
2023-03-02 16:59:05 +0100

    FactoriaDone

--- src/simulator/factories/MovingTowardsFixedPointBuilder.java
+++ src/simulator/factories/MovingTowardsFixedPointBuilder.java
@@ -2,12 +2,15 @@ package simulator.factories;
 
 import org.json.JSONObject;
 
+import simulator.misc.Vector2D;
 import simulator.model.ForceLaws;
+import simulator.model.MovingBody;
+import simulator.model.MovingTowardsFixedPoint;
 
 public class MovingTowardsFixedPointBuilder extends Builder<ForceLaws>{
 
 	public MovingTowardsFixedPointBuilder() {
-		super(null, null);
+		super("mtfp", "force");
 	}
 	public MovingTowardsFixedPointBuilder(String typeTag, String desc) {
 		super(typeTag, desc);
@@ -16,7 +19,17 @@ public class MovingTowardsFixedPointBuilder extends Builder<ForceLaws>{
 	@Override
 	protected ForceLaws createInstance(JSONObject data) {
 		// TODO Auto-generated method stub
-		return null;
+		for (String key : data.keySet()) {
+			if(key == null) 
+				throw new IllegalArgumentException(); 
+		}	
+		
+		
+		//MovingBody(String id, String gid, Vector2D v, Vector2D p, double m)
+		Vector2D c = new Vector2D(data.getJSONArray("c").getDouble(0), data.getJSONArray("c").getDouble(1));
+		Double g = data.getDouble("g");
+		
+		return new MovingTowardsFixedPoint(c, g);
 	}
 
 }
