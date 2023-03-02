60beaf1236fdfae2f32dde632e63c423caad01de
fernandoclz <114144548+fernandoclz@users.noreply.github.com>
2023-03-02 20:22:56 +0100

    Un triste intento más

--- src/simulator/control/Controller.java
+++ src/simulator/control/Controller.java
@@ -1,5 +1,22 @@
 package simulator.control;
 
-public class Controller {
+import java.io.InputStream;
+import java.io.OutputStream;
 
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+
+public class Controller {
+	public void loadData(InputStream in) {
+		JSONObject jsonInupt = new JSONObject(new JSONTokener(in));
+		
+		String g[] = JSONObject.getNames(jsonInupt) ;
+		for(String group: g) {
+			 
+		}
+	}
+	public void run(int n, OutputStream out) {
+		
+	}
 }
